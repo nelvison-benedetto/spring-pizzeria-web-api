ca7af71753408877522bfca53f259d7a66776afa
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-03-18 19:41:31 +0100

    project setup

---
+++ src/test/java/org/lessons/java/spring_pizzeriacrud/SpringPizzeriacrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_pizzeriacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPizzeriacrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
